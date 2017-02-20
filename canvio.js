213fc3bfda8034cd2fe7b0c19c403d00cffe5a70
ArtOfCode- <owen.jenkins2903@gmail.com>
2017-02-20 03:08:57 +0000

    Basic turtle implementation

--- canvio.js
+++ canvio.js
@@ -39,11 +39,77 @@ canvio.Image = function() {
     },
 
     setSource: function(url) {
-      var img = document.createElement("image");
+      var img = new Image();
       img.onload = function() {
         ctx.drawImage(img, 0, 0);
       };
       img.src = url;
     }
   };
-}
+};
+
+canvio.Turtle = function(context) {
+  var ctx = context;
+  var color = "black";
+  var penState = "down";
+
+  var pos = {
+    x: ctx.canvas.width / 2,
+    y: ctx.canvas.height / 2
+  };
+
+  var heading = 0;
+
+  function rad(deg) {
+    return deg * (Math.PI / 180);
+  }
+
+  function tox(hdg) {
+    return ((hdg + 90) % 360) - 180;
+  }
+
+  function dx(tx, d) {
+    return d * Math.cos(rad(tx));
+  }
+
+  function dy(tx, d) {
+    return d * Math.sin(rad(tx));
+  }
+
+  ctx.beginPath();
+  ctx.moveTo(pos.x, pos.y);
+
+  return {
+    penDown: function() {
+      penState = "down";
+    },
+
+    penUp: function() {
+      penState = "up";
+    },
+
+    forward: function(amount) {
+      pos.x += dx(tox(heading), amount);
+      pos.y += dy(tox(heading), amount);
+      if (penState == "down") {
+        ctx.lineTo(pos.x, pos.y);
+        ctx.stroke();
+      }
+      else {
+        ctx.moveTo(pos.x, pos.y);
+      }
+    },
+
+    backward: function(amount) {
+      this.forward(-amount);
+    },
+
+    left: function(turn) {
+      heading = (heading + (360 - turn)) % 360;
+    },
+
+    right: function(turn) {
+      heading = (heading + turn) % 360;
+    }
+  };
+};
